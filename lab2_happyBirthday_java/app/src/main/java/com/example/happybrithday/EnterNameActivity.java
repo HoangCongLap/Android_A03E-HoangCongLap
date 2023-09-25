da971d03d21918efe200bba53172fe744a6598a6
HoangCongLap <lapconghoang@gmail.com>
2023-09-25 13:17:14 +0700

    new

--- lab2_happyBirthday_java/app/src/main/java/com/example/happybrithday/EnterNameActivity.java
+++ lab2_happyBirthday_java/app/src/main/java/com/example/happybrithday/EnterNameActivity.java
@@ -3,13 +3,14 @@ package com.example.happybrithday;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class EnterNameActivity extends AppCompatActivity {
+Button Login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_enter_name);
 
     }
 }
